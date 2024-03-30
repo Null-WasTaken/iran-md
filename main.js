a3f789470a93e90dc4c3c7906448cc205a9236c1
Amir Hossein Fallah <notrealamir1385@gmail.com>
2024-03-30 06:19:52 -0700

    cdklj

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
 let IsSwitch = false;
 
-document.addEventListener(onclick ,()=> {
-    document.getElementsByClassName
-});
+function SwitchLogin() {
+    IsSwitch = true;
+
+    if (IsSwitch == true) {
+        document.getElementById("signup").classList.add("none");
+        document.getElementById("login").classList.remove("none");
+        document.getElementById("switcher-signup").classList.add("not-active");
+        document.getElementById("switcher-login").classList.remove("not-active");
+    }
+}
+
+function SwtichSignUp() {
+    IsSwitch = false;
+
+    if (IsSwitch == false) {
+        document.getElementById("login").classList.add("none");
+        document.getElementById("signup").classList.remove("none");
+        document.getElementById("switcher-signup").classList.remove("not-active");
+        document.getElementById("switcher-login").classList.add("not-active");
+    }
+}
